438eaec77dd4dd97c0329685f8f0c8e546393651
jblivey92 <jazzmineschmidt@gmail.com>
2025-07-16 19:46:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("weather").textContent = "☀️ 75°F, Clear";
+document.getElementById("pollen").textContent = "🌸 Pollen: Moderate";
+document.getElementById("sun").textContent = "🌅 Sunrise: 5:41 AM | 🌇 Sunset: 8:47 PM";
